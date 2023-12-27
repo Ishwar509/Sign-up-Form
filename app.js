16e522f173015b2e16729e9d26e3230c7919feed
ishwar_suthar <123794969+Ishwar509@users.noreply.github.com>
2023-12-27 16:08:20 +0530

    Add form validation

--- app.js
+++ app.js
@@ -1,20 +1,115 @@
+const form = document.querySelector('form');
+const firstName = document.querySelector('#firstName');
+const lastName = document.querySelector('#lastName');
+const mail = document.querySelector('#mail');
+const phoneNumber = document.querySelector('#phoneNumber');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
 
-const registrationForm = document.querySelector('form');
-registrationForm.addEventListener('submit', check);
+const mailError = document.querySelector('#mail + .error');
+const phoneNumberError = document.querySelector('#phoneNumber + .error');
+const passwordError = document.querySelector('#password + .error');
+const confirmPasswordError = document.querySelector('#confirm-password + .error');
 
 
-function check(event) {
-    event.preventDefault();
+const emptyFirstNameMessage = 'Please enter your first name';
+const emptyLastNameMessage = 'Please enter your last name';
+const emptyMailMessage = 'Please enter your E-mail';
+const emptyPhoneMessage = 'Please enter your Phone Number';
+const emptyPasswordMessage = 'Please enter a password';
 
-    const password = document.querySelector('#password').value;
-    const confirmPassword = document.querySelector('#confirm-password').value;
-    const errorMessage = document.querySelector('.error');
+firstName.setCustomValidity(emptyFirstNameMessage);
+lastName.setCustomValidity(emptyLastNameMessage);
+mail.setCustomValidity(emptyMailMessage);
+phoneNumber.setCustomValidity(emptyPhoneMessage);
+password.setCustomValidity(emptyPasswordMessage);
 
-    if (password !== confirmPassword) {
-        errorMessage.style.visibility = 'visible';
+firstName.addEventListener('input', () => {
+    firstName.value = firstName.value.trim();
+
+    if(firstName.validity.valueMissing){
+        firstName.setCustomValidity(emptyFirstNameMessage);
     }
-    else {
-        alert("Registration Successfull");
-        errorMessage.style.visibility = 'hidden';
+    else{
+        firstName.setCustomValidity('');
     }
-}
+});
+
+lastName.addEventListener('input', () => {
+    lastName.value = lastName.value.trim();
+
+    if(lastName.validity.valueMissing){
+        lastName.setCustomValidity(emptyLastNameMessage);
+    }
+    else{
+        lastName.setCustomValidity('');
+    }
+});
+
+mail.addEventListener('input', () => {
+
+    if(mail.validity.valueMissing){
+        mail.setCustomValidity(emptyMailMessage);
+        mailError.textContent = '';
+        return;
+    }
+    if(mail.validity.patternMismatch){
+        mail.setCustomValidity('Invalid Format');
+        mailError.textContent = 'Please match the proper format';
+        return;
+    }
+
+    mail.setCustomValidity('');
+    mailError.textContent = '';
+});
+
+phoneNumber.addEventListener('input', () => {
+    if(phoneNumber.validity.valueMissing){
+        phoneNumber.setCustomValidity(emptyPhoneMessage);
+        phoneNumberError.textContent = '';
+        return;
+    }
+    if(phoneNumber.validity.patternMismatch){
+        phoneNumber.setCustomValidity('Invalid Format');
+        phoneNumberError.textContent = 'Please match the proper format';
+        return;
+    }
+
+    phoneNumber.setCustomValidity('');
+    phoneNumberError.textContent = '';
+});
+
+password.addEventListener('input', () => {
+    if(password.validity.valueMissing){
+        password.setCustomValidity(emptyPasswordMessage);
+        passwordError.textContent = '';
+        return;
+    }
+    if(password.validity.tooShort){
+        password.setCustomValidity('Password length must be between 8 - 16 characters');
+        passwordError.textContent = 'Password is too short';
+        return;
+    }
+
+    password.setCustomValidity('');
+    passwordError.textContent = '';
+});
+
+confirmPassword.addEventListener('input', () => {
+    if(confirmPassword.value !== password.value){
+        confirmPasswordError.textContent = 'Password does not match'
+        confirmPassword.setCustomValidity('Password does not match');
+        return;
+    }
+    
+    confirmPasswordError.textContent = '';
+    confirmPassword.setCustomValidity('');
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if(form.reportValidity()){
+        alert("Form submitted");
+    }
+});
